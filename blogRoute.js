f13b2d704381890b7db6b25b99aaa1e547096c95
Zhong86 <billy3rlvin@gmail.com>
2025-12-23 21:16:16 +0700

    Update, Get, Get All

--- blogRoute.js
+++ blogRoute.js
@@ -1,10 +1,87 @@
 const express = require('express'); 
 const router = express.Router(); 
+const fs = require('fs'); 
+const path = './data.json'; 
 
-router.get('/blog', (req, res) => {
-    const blog = {
-        
-    };
+router.post('/posts', getBlogs, (req, res) => {
+  console.log(blogs); 
+  const blog = {
+    title: req.body.title, 
+    content: req.body.content, 
+    category: req.body.category, 
+    tags: req.body.tags, 
+    createdAt: new Date.now(), 
+    updatedAt: new Date.now()
+  };
+
+  blogs.push(blog); 
+
+  try {
+    fs.writeFileSync(path, JSON.stringify(blog, null, 2)); 
+    res.status(201).json(blogs);
+  } catch (error) {
+    res.status(400).json({message: error.message}); 
+  }
 });
+
+router.put('/posts/:id', getBlogs, (req, res) => {
+  const postInfo = { 
+    title: req.body.title, 
+    content: req.body.content, 
+    category: req.body.category, 
+    tags: req.body.tags, 
+  }; 
+
+  try {
+    let updatedPost = {}; 
+    (res.blogs).forEach(post => {
+      if (post.id === req.params.id) {
+        post.title = postInfo.title;
+        post.content = postInfo.content; 
+        post.category = postInfo.category; 
+        post.tags = postInfo.tags; 
+        post.updatedAt = new Date.now(); 
+
+        updatedPost = post; 
+      }
+    });
+    
+    fs.writeFileSync(path, JSON.stringify([res.blogs], null, 2); 
+    res.status(200).json(updatedPost); 
+  } catch (error) {
+    res.status(404).json({message: 'Blog ID not found'}); 
+  }
+}); 
+
+router.get('/posts/:id', getBlogs, (req, res) => {
+  try {
+    (res.blogs).forEach(blog => {
+      if (blog.id === req.params.id) {
+        return res.status(200).json(blog);
+      }
+    });
+  } catch (error) {
+    res.status(404).json({message: 'Blog post ID not found'}); 
+  }
+}); 
+
+router.get('/posts', getBlogs, (req, res) => {
+  try {
+    return res.status(200).json(res.blogs); 
+  } catch (error) {
+    res.status(500).json({message: error.message}); 
+  }
+}); 
+
+function getBlogs(req, res, next) {
+  try {
+    if(!fs.existsSync(path)) return res.status(404).json({message: 'File not found'}); 
+    blogs = fs.readFileSync(path, 'utf8'); 
+  } catch (error) {
+    return res.status(500).json({message: error.message}); 
+  }
+  res.blogs = blogs;
+  next(); 
+}
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const port = process.env.PORT || 8080; 
 const app = express(); 
 
-const blogRoute = require('blogRoute'); 
+const blogRoute = require('./blogRoute'); 
 app.use('/api', blogRoute);
 app.listen(port, () => {
     console.log('Server connected');
